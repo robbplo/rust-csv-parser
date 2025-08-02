daf3ba15266bf052cbd9a15a014d957225167913
Robbin Ploeger <robbplo@gmail.com>
2025-08-02 09:44:14 +0200

    implement fields with multiple chars

--- src/tokenizer/tokenizer.rs
+++ src/tokenizer/tokenizer.rs
@@ -26,22 +26,53 @@ impl<'a> Iterator for CsvTokenizer<'a> {
     type Item = CsvToken<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let bytes = self.input.as_bytes();
-        let token = match bytes.get(self.position) {
-            Some(b'\n') => Some(CsvToken::Newline),
+        let token = match self.current() {
+            Some(b'\n') => {
+                self.position += 1;
+                Some(CsvToken::Newline)
+            }
             Some(char) => {
                 if *char == self.delimiter as u8 {
+                    self.position += 1;
                     Some(CsvToken::Delimiter)
                 } else {
-                    Some(CsvToken::Field(
-                        &self.input[self.position..self.position + 1],
-                    ))
+                    let length = self.chars_until_end_of_field();
+                    let result = Some(CsvToken::Field(
+                        &self.input[self.position..self.position + length],
+                    ));
+                    self.position += length;
+                    result
                 }
             }
             None => None,
         };
-        self.position += 1;
         token
+    }
+}
+
+impl CsvTokenizer<'_> {
+    fn current(&self) -> Option<&u8> {
+        self.input.as_bytes().get(self.position)
+    }
+
+    fn get_offset(&self, i: usize) -> Option<&u8> {
+        self.input.as_bytes().get(self.position + i)
+    }
+
+    fn chars_until_end_of_field(&self) -> usize {
+        let mut i = 1;
+        loop {
+            match self.get_offset(i) {
+                None => break,
+                Some(byte) => {
+                    if *byte as char == self.delimiter || *byte == b'\n' {
+                        break;
+                    }
+                }
+            }
+            i += 1;
+        }
+        i
     }
 }
 
@@ -52,12 +83,27 @@ fn it_handles_empty_input() {
 }
 
 #[test]
-fn it_handles_single_char_fields_and_delimiters() {
-    let mut t = CsvTokenizer::new("1,2,3", ',');
+fn it_handles_single_char_fields_with_newlines_and_delimiters() {
+    let mut t = CsvTokenizer::new("1,2,3\n4,5,6", ',');
     assert_eq!(t.next(), Some(CsvToken::Field("1")));
     assert_eq!(t.next(), Some(CsvToken::Delimiter));
     assert_eq!(t.next(), Some(CsvToken::Field("2")));
     assert_eq!(t.next(), Some(CsvToken::Delimiter));
     assert_eq!(t.next(), Some(CsvToken::Field("3")));
+    assert_eq!(t.next(), Some(CsvToken::Newline));
+    assert_eq!(t.next(), Some(CsvToken::Field("4")));
+    assert_eq!(t.next(), Some(CsvToken::Delimiter));
+    assert_eq!(t.next(), Some(CsvToken::Field("5")));
+    assert_eq!(t.next(), Some(CsvToken::Delimiter));
+    assert_eq!(t.next(), Some(CsvToken::Field("6")));
+    assert_eq!(t.next(), None);
+}
+
+#[test]
+fn it_handles_multiple_char_fields() {
+    let mut t = CsvTokenizer::new("abc,def", ',');
+    assert_eq!(t.next(), Some(CsvToken::Field("abc")));
+    assert_eq!(t.next(), Some(CsvToken::Delimiter));
+    assert_eq!(t.next(), Some(CsvToken::Field("def")));
     assert_eq!(t.next(), None);
 }
